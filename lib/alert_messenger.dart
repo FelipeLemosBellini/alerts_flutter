46d5215027fd8507a9354b036a725fd92c72546d
Felipe Bellini <felipe.bellini@krykto.com>
2024-06-24 20:48:06 -0300

    set order alerts set animated for show alert

--- lib/alert_messenger.dart
+++ lib/alert_messenger.dart
@@ -8,6 +8,7 @@ enum AlertPriority {
   info(0);
 
   const AlertPriority(this.value);
+
   final int value;
 }
 
@@ -29,38 +30,30 @@ class Alert extends StatelessWidget {
   Widget build(BuildContext context) {
     final statusbarHeight = MediaQuery.paddingOf(context).top;
     return Material(
-      child: Ink(
-        color: backgroundColor,
-        height: kAlertHeight + statusbarHeight,
-        child: Column(
-          children: [
-            SizedBox(height: statusbarHeight),
-            Expanded(
-              child: Row(
-                children: [
-                  const SizedBox(width: 28.0),
-                  IconTheme(
-                    data: const IconThemeData(
-                      color: Colors.white,
-                      size: 36,
-                    ),
-                    child: leading,
+        child: Ink(
+            color: backgroundColor,
+            height: kAlertHeight + statusbarHeight,
+            child: Column(children: [
+              SizedBox(height: statusbarHeight),
+              Expanded(
+                  child: Row(children: [
+                const SizedBox(width: 28.0),
+                IconTheme(
+                  data: const IconThemeData(
+                    color: Colors.white,
+                    size: 36,
                   ),
-                  const SizedBox(width: 16.0),
-                  Expanded(
+                  child: leading,
+                ),
+                const SizedBox(width: 16.0),
+                Expanded(
                     child: DefaultTextStyle(
-                      style: const TextStyle(color: Colors.white),
-                      child: child,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            const SizedBox(width: 28.0),
-          ],
-        ),
-      ),
-    );
+                  style: const TextStyle(color: Colors.white),
+                  child: child,
+                ))
+              ])),
+              const SizedBox(width: 28.0),
+            ])));
   }
 }
 
@@ -91,11 +84,14 @@ class AlertMessenger extends StatefulWidget {
   }
 }
 
-class AlertMessengerState extends State<AlertMessenger> with TickerProviderStateMixin {
+class AlertMessengerState extends State<AlertMessenger> with SingleTickerProviderStateMixin {
   late final AnimationController controller;
   late final Animation<double> animation;
 
   Widget? alertWidget;
+  GroupAlertsByValue _groupAlertsByValue = GroupAlertsByValue();
+
+  bool isShowingTheAlert = false;
 
   @override
   void initState() {
@@ -105,6 +101,14 @@ class AlertMessengerState extends State<AlertMessenger> with TickerProviderState
       duration: const Duration(milliseconds: 300),
       vsync: this,
     );
+
+    controller.addStatusListener((AnimationStatus status) {
+      if (status == AnimationStatus.dismissed) {
+        setState(() {
+          _verifyOtherAlerts();
+        });
+      }
+    });
   }
 
   @override
@@ -123,42 +127,57 @@ class AlertMessengerState extends State<AlertMessenger> with TickerProviderState
   }
 
   void showAlert({required Alert alert}) {
-    setState(() => alertWidget = alert);
+    if (isShowingTheAlert) {
+      _saveAlert(alert);
+    } else {
+      setState(() => alertWidget = alert);
+      isShowingTheAlert = true;
+    }
     controller.forward();
   }
 
   void hideAlert() {
+    isShowingTheAlert = false;
+
     controller.reverse();
+  }
+
+  void _verifyOtherAlerts() {
+    Alert? alert;
+    alert = _groupAlertsByValue.nextAlert();
+    if (alert != null) {
+      showAlert(alert: alert);
+    }
+  }
+
+  void _saveAlert(Alert alert) {
+    if (alert.priority.value == 2) {
+      _groupAlertsByValue.listError.add(alert);
+    } else if (alert.priority.value == 1) {
+      _groupAlertsByValue.listWarning.add(alert);
+    } else {
+      _groupAlertsByValue.listInfo.add(alert);
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    final statusbarHeight = MediaQuery.of(context).padding.top;
+    final statusbarHeight = MediaQuery.paddingOf(context).top;
 
     return AnimatedBuilder(
-      animation: animation,
-      builder: (context, child) {
-        final position = animation.value + kAlertHeight;
-        return Stack(
-          clipBehavior: Clip.antiAliasWithSaveLayer,
-          children: [
+        animation: animation,
+        builder: (context, child) {
+          final position = animation.value + kAlertHeight;
+          return Stack(clipBehavior: Clip.antiAliasWithSaveLayer, children: [
             Positioned.fill(
-              top: position <= statusbarHeight ? 0 : position - statusbarHeight,
-              child: _AlertMessengerScope(
-                state: this,
-                child: widget.child,
-              ),
-            ),
-            Positioned(
-              top: animation.value,
-              left: 0,
-              right: 0,
-              child: alertWidget ?? const SizedBox.shrink(),
-            ),
-          ],
-        );
-      },
-    );
+                top: position <= statusbarHeight ? 0 : position - statusbarHeight,
+                child: _AlertMessengerScope(
+                  state: this,
+                  child: widget.child,
+                )),
+            Positioned(top: animation.value, left: 0, right: 0, child: alertWidget ?? const SizedBox.shrink())
+          ]);
+        });
   }
 }
 
@@ -181,5 +200,35 @@ class _AlertMessengerScope extends InheritedWidget {
     final scope = maybeOf(context);
     assert(scope != null, 'No _AlertMessengerScope found in context');
     return scope!;
+  }
+}
+
+class GroupAlertsByValue {
+  List<Alert> listError = [];
+  List<Alert> listWarning = [];
+  List<Alert> listInfo = [];
+
+  GroupAlertsByValue();
+
+  bool get listErrorIsEmpty => listError.isEmpty;
+
+  bool get listWarningIsEmpty => listWarning.isEmpty;
+
+  bool get listInfoIsEmpty => listInfo.isEmpty;
+
+  Alert? nextAlert() {
+    Alert? alert;
+
+    if (!listErrorIsEmpty) {
+      alert = listError.first;
+      listError.remove(listError.first);
+    } else if (!listWarningIsEmpty) {
+      alert = listWarning.first;
+      listWarning.remove(listWarning.first);
+    } else if (!listInfoIsEmpty) {
+      alert = listInfo.first;
+      listInfo.remove(listInfo.first);
+    }
+    return alert;
   }
 }
